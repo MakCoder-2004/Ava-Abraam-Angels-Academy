63954fa6a125bffd299bfa8063e7b45c3252491b
Makrious-Ayman <maaakrious2004@gmail.com>
2025-06-20 02:43:42 +0300

    Added Metadata to the website

---
+++ src/app/client-layout.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Navbar from "@/components/Navbar";
+import "../Css/globals.css";
+import Footer from "@/components/Footer";
+import { LoadingProvider, useLoading } from "@/contexts/LoadingContext";
+
+
+function LayoutContent({ children }: { children: React.ReactNode }) {
+  const { isLoading } = useLoading();
+
+  return (
+    <>
+      <Navbar />
+      {children}
+      {!isLoading && <Footer />}
+    </>
+  );
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-orange-100/50">
+        <LoadingProvider>
+          <LayoutContent>{children}</LayoutContent>
+        </LoadingProvider>
+      </body>
+    </html>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,15 @@
-"use client";
+import type { Metadata } from "next";
+import ClientLayout from "./client-layout";
 
-import Navbar from "@/components/Navbar";
-import "../Css/globals.css";
-import Footer from "@/components/Footer";
-import { LoadingProvider, useLoading } from "@/contexts/LoadingContext";
-
-function LayoutContent({ children }: { children: React.ReactNode }) {
-  const { isLoading } = useLoading();
-
-  return (
-    <>
-      <Navbar />
-      {children}
-      {!isLoading && <Footer />}
-    </>
-  );
-}
+export const metadata: Metadata = {
+  title: "Ava Abraam Angels Academy",
+  description: "Welcome To Our Page!",
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className="bg-orange-100/50">
-        <LoadingProvider>
-          <LayoutContent>{children}</LayoutContent>
-        </LoadingProvider>
-      </body>
-    </html>
-  );
+  return <ClientLayout>{children}</ClientLayout>;
 }
